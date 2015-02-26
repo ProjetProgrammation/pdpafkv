e7cc0beb184182bc17e66a67e07375a397145c8e
Arrestier <aresjeje@hotmail.fr>
2015-02-26 17:01:52 +0100

    Plusieurs modification son/interface avec un min sur le test

--- src/GUI/SonGUI.java
+++ src/GUI/SonGUI.java
@@ -25,9 +25,9 @@ import javafx.scene.media.MediaPlayer;
  */
 public class SonGUI extends Parent {
     
-    final URL resource = getClass().getResource("mario.mp3");
-    final Media media = new Media(resource.toString());
-    final MediaPlayer mediaPlayer = new MediaPlayer(media); 
+    final URL mario = getClass().getResource("mario.mp3");
+    final URL zelda = getClass().getResource("zelda.mp3");
+    final URL ff7 = getClass().getResource("ff7.mp3"); 
     
     
     public SonGUI(Language langSel){
@@ -41,32 +41,53 @@ public class SonGUI extends Parent {
         FlowPane fond_son = new FlowPane();
         fond_son.setVgap(8);
         fond_son.setHgap(4);
+        fond_son.autosize();
         //fond_son.setPrefWrapLength(300);
-        fond_son.setPadding(new Insets(15, 12, 15, 12));
+        //fond_son.setPadding(new Insets(30, 24, 30, 24));
         fond_son.setStyle("-fx-background-color: #336699; -fx-border-color: #000000;");
 
-        //Fichier Aucio
+        //Fichier Aucio1
         javafx.scene.control.Button b1 = new javafx.scene.control.Button();
-        b1.setText("Read");
+        b1.setText("Sound 1");
         
         //Action du bouton
         b1.setOnAction(new EventHandler<ActionEvent>() {    
             @Override
             public void handle(ActionEvent event) {
-                    appuyer();
+                    appuyer(mario);
+            }
+        });
+        
+        //Fichier Aucio2
+        javafx.scene.control.Button b2 = new javafx.scene.control.Button();
+        b2.setText("Sound 2");
+        
+        //Action du bouton
+        b2.setOnAction(new EventHandler<ActionEvent>() {    
+            @Override
+            public void handle(ActionEvent event) {
+                    appuyer(zelda);
+            }
+        });
+        
+        //Fichier Aucio3
+        javafx.scene.control.Button b3 = new javafx.scene.control.Button();
+        b3.setText("Sound 3");
+        
+        //Action du bouton
+        b3.setOnAction(new EventHandler<ActionEvent>() {    
+            @Override
+            public void handle(ActionEvent event) {
+                    appuyer(ff7);
             }
         });
         
         //création des boutons
         ToggleGroup groupe = new ToggleGroup();
         RadioButton b4 = new RadioButton();
-        RadioButton b2 = new RadioButton();
-        RadioButton b3 = new RadioButton();
         RadioButton b5 = new RadioButton();
         RadioButton b6 = new RadioButton();
         b4.setToggleGroup(groupe);
-        b2.setToggleGroup(groupe);
-        b3.setToggleGroup(groupe);
         b5.setToggleGroup(groupe);
         b6.setToggleGroup(groupe);
         b1.setFocusTraversable(false);
@@ -80,7 +101,7 @@ public class SonGUI extends Parent {
         GridPane zoneSon = new GridPane();
                 
         //on ajoute les boutons au layout
-        zoneSon.setPrefSize(200, 200);
+        zoneSon.autosize();
         zoneSon.add(b1, 0, 0);
         zoneSon.add(b2, 1, 0);
         zoneSon.add(b3, 2, 0);
@@ -93,7 +114,9 @@ public class SonGUI extends Parent {
         fond_son.getChildren().add(zoneSon);
         this.getChildren().add(fond_son);
    }
-    public void appuyer(){
-            mediaPlayer.play();
+    public void appuyer(URL url){
+    final Media media = new Media(url.toString());
+    final MediaPlayer mediaPlayer = new MediaPlayer(media);
+    mediaPlayer.play();
     } 
 }
