be0e5ff85a7f919cbbd3a87c5546afa06e16e58e
Fabre Thibaut <fabre.thibaut@gmail.com>
2015-03-14 19:43:36 +0100

    Question à afficher Num 3

--- src/BDD/Test.java
+++ src/BDD/Test.java
@@ -15,39 +15,6 @@ public class Test {
 	public static void main(String[] args) {
 		DataBase db = new DataBase();
 		db.createTables();
-		db.addLanguage("French");
-		db.addLanguage("English");
-		db.addAudio("seth","Audio\\seth.mp3","mp3","French");
-                db.addAudio("Ska-P-Canabis","Audio\\Ska-P-Canabis.mp3", "mp3","French");
-                db.addAudio("cosmo","Audio\\cosmo.mp3","mp3","French");
-                db.addAudio("wasted","Audio\\wasted.mp3","mp3","French");
-                db.addAudio("mark","Audio\\mark.mp3","mp3","French");
-		db.addVideo("2013_3_19_S29_fr_L1_ADMI_B_ok","Video\\2013_3_19_S29_fr_L1_ADMI_B_ok.mp4","mp4","French");
-		db.addVideo("YouTube-Compilation-des-repliques-de-Kadoc","Video\\YouTube-Compilation-des-repliques-de-Kadoc.flv","flv","English");
-		Language tempLanguage = new Language(1, "French");
-                System.out.println(tempLanguage.toString());
-		Video tempVideo = new Video(db.manageVideo(tempLanguage));
-                System.out.println(tempVideo.toString());
-		Audio tempAudio = new Audio(db.manageAudio(tempLanguage));
-		//System.out.println(db.searchLanguageByName("French"));
-		//System.out.println("Audio recherché :");
-		//System.out.println(tempAudio);
-		//System.out.println("Audio trouvé :");
-		//System.out.println(db.searchAudioByNameFormat(tempAudio.getName(), tempAudio.getFormat()) + "\n");
-		//System.out.println("Vidéo recherchée :");
-		//System.out.println(tempVideo);
-		//System.out.println("Vidéo trouvée :");
-		//System.out.println(db.searchVideoByNameFormat(tempVideo.getName(), tempVideo.getFormat()));
-		String tempQuestion1 = new String("C'est ici le ramonage de cheminée ?");
-		String tempQuestion2 = new String("On dit merci qui ?");
-                db.addQuestion(tempQuestion2, tempVideo, tempAudio, "French");
-                db.addQuestion(tempQuestion1, tempVideo, tempAudio, "English");
-		//System.out.println(db.manageVideo(tempLanguage));
-		//System.out.println(db.manageAudio(tempLanguage));
-		//System.out.println(db.manageQuestion(tempLanguage));
-		System.out.println(db.getAllLanguages());
-		System.out.println(db.getAllAudios());
-		System.out.println(db.getAllVideos());
-		System.out.println(db.getAllQuestions());
+
 	}
 }
--- test/Test/TestDB.java
+++ test/Test/TestDB.java
@@ -1,57 +1,51 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Cette classe sert à tester la classe DataBase
+ * Attention à bien ajouter les langues en premier
  */
-package Test;
 
-import BDD.DataBase;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+package Test;
+import BDD.*;
 
 /**
  *
- * @author Thibaut
+ * @author akervadec
  */
 public class TestDB {
-    
-    DataBase db;
-    
-    public TestDB(){
-            Connection c = null;
-	    Statement stmt = null;
-	    try {
-	      Class.forName("org.sqlite.JDBC");
-	      c = DriverManager.getConnection("jdbc:sqlite:dataBase.db");
-	      System.out.println("Opened database successfully");
 
-	      stmt = c.createStatement();
-                ResultSet rs = stmt.executeQuery( "SELECT * FROM Audio;" );
-                while ( rs.next() ) {
-                   int id = rs.getInt("id");
-                   String  name = rs.getString("name");
-                   String  path = rs.getString("file_path");
-                   String  language = rs.getString("language");
-                   String  format = rs.getString("format");
-                   System.out.println( "ID = " + id );
-                   System.out.println( "NAME = " + name );
-                   System.out.println( "path = " + path );
-                   System.out.println( "language = " + language );
-                   System.out.println( "format = " + format );
-                   System.out.println();
-                }
-              rs.close();
-	      stmt.close();
-	      c.close();
-	    } catch ( Exception e ) {
-	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	      System.exit(0);
-	    }        
-    }
-          
-    
-    
+	public static void main(String[] args) {
+		DataBase db = new DataBase();
+		db.createTables();
+		db.addLanguage("French");
+		db.addLanguage("English");
+		db.addAudio("seth","Audio\\seth.mp3","mp3","French");
+                db.addAudio("Ska-P-Canabis","Audio\\Ska-P-Canabis.mp3", "mp3","French");
+                db.addAudio("cosmo","Audio\\cosmo.mp3","mp3","French");
+                db.addAudio("wasted","Audio\\wasted.mp3","mp3","French");
+                db.addAudio("mark","Audio\\mark.mp3","mp3","French");
+		db.addVideo("2013_3_19_S29_fr_L1_ADMI_B_ok","Video\\2013_3_19_S29_fr_L1_ADMI_B_ok.mp4","mp4","French");
+		db.addVideo("YouTube-Compilation-des-repliques-de-Kadoc","Video\\YouTube-Compilation-des-repliques-de-Kadoc.flv","flv","English");
+		Language tempLanguage = new Language(1, "French");
+		Video tempVideo = new Video(db.manageVideo(tempLanguage));
+		Audio tempAudio = new Audio(db.manageAudio(tempLanguage));
+		//System.out.println(db.searchLanguageByName("French"));
+		//System.out.println("Audio recherché :");
+		//System.out.println(tempAudio);
+		//System.out.println("Audio trouvé :");
+		//System.out.println(db.searchAudioByNameFormat(tempAudio.getName(), tempAudio.getFormat()) + "\n");
+		//System.out.println("Vidéo recherchée :");
+		//System.out.println(tempVideo);
+		//System.out.println("Vidéo trouvée :");
+		//System.out.println(db.searchVideoByNameFormat(tempVideo.getName(), tempVideo.getFormat()));
+		//String tempQuestion1 = new String("C'est ici le ramonage de cheminée ?");
+		//String tempQuestion2 = new String("On dit merci qui ?");
+                db.addQuestion("heureux?", tempVideo, tempAudio, "French");
+                db.addQuestion("colère?", tempVideo, tempAudio, "English");
+		//System.out.println(db.manageVideo(tempLanguage));
+		//System.out.println(db.manageAudio(tempLanguage));
+		//System.out.println(db.manageQuestion(tempLanguage));
+		System.out.println(db.getAllLanguages());
+		System.out.println(db.getAllAudios());
+		System.out.println(db.getAllVideos());
+		System.out.println(db.getAllQuestions());
+	}
 }
