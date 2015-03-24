710e4458c2a0e0d47bff6f4f7dbdb9dfd10f114b
Alexandre Kervadec <alexandre.kervadec@gmail.com>
2015-03-24 22:12:56 +0100

    Administration | Mise en place (prémices...)

---
+++ src/Admin/AdminDatabase.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Admin;
+
+import BDD.DataBase;
+
+/**
+ * Cette classe permet de gérer l'administration de la base de données
+ * @author alexandre
+ */
+public class AdminDatabase {
+    
+    private DataBase db;
+    
+    
+    
+}
