8f7caa26e576fccf1efb5d9dc460ecc5c8994bd7
khelixx <guillaume21@192.168.1.39>
2015-03-28 09:21:51 +0100

    package Errors anglisheuh

--- src/Errors/Errors.java
+++ src/Errors/Errors.java
@@ -7,83 +7,97 @@ package Errors;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * Find different types of error on programs or system for example.
  *
  * @author guillaume
  */
 public class Errors extends Exception {
-    
-    public Errors(){}
-    
-    public String errorsMessages(Object message){
-        
-        String mess = null;
-      
-        if (message instanceof String){
-            Pattern p = Pattern .compile("^[a-zA-Z]+$");
-            Matcher m = p.matcher((String)message);
-            while (m.find()){
-                return null;
-            }
-        mess="faux";
-        }
-         
-        else if(message instanceof Integer){
-            if ((int)message > 125){
-                mess="faux";
-            }
-            else{
-                return null;
-            }
-         
-        }
-        return mess;
+
+    /**
+     * Constructs a Errors object.
+     */
+    public Errors() {
     }
-    
-    public String errorDate(String date){
-        String s = null;
+
+    /**
+     * Find errors on last name, first name or mother tongue.
+     *
+     * @param valueTested null if no errors.
+     * @return String.
+     */
+    public String errorsMessages(Object valueTested) {
+
+        String messageError = null;
+
+        if (valueTested instanceof String) {
+            Pattern p = Pattern.compile("^[a-zA-Z]+$");
+            Matcher m = p.matcher((String) valueTested);
+            while (m.find()) {
+                return null;
+            }
+            messageError = "false";
+        } else if (valueTested instanceof Integer) {
+            if ((int) valueTested > 125) {
+                messageError = "false";
+            } else {
+                return null;
+            }
+
+        }
+        return messageError;
+    }
+
+    /**
+     * Errors on birthday.
+     *
+     * @param dateTested birthday of the user.
+     * @return String.
+     */
+    public String errorDate(String dateTested) {
+        String errorMessage = null;
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Date d = new Date();
-        
+        Date d;
+
         try {
-            d = sdf.parse(date);
+            d = sdf.parse(dateTested);
             String t = sdf.format(d);
-            if(t.compareTo(date) !=  0)
-                s="faux";
-            else
+            if (t.compareTo(dateTested) != 0) {
+                errorMessage = "false";
+            } else
                 ;
         } catch (Exception e) {
-                s= "faux";
+            errorMessage = "false";
         }
-        
-        return s;
+
+        return errorMessage;
     }
-    
-    public void errorsOs(){
-        String name = System.getProperty ( "os.name" );
-        String version = System.getProperty ( "os.version" );
-        String java = System.getProperty("java.home");
-        
-        
-         if (name.contains("Windows") || name.contains("MAC") || name.contains("Linux")){}
-         else{
-                 System.out.println("Votre système d'exploitation n'est pas pris en charge");
-         }
-         
-         
-         
-         Pattern p = Pattern.compile("jdk1.8.0_31");
-         Matcher m = p.matcher(java);
-         if (m.find()){
-             System.out.println("a jour");;
-         }
-         else{
-             System.out.println("Il faut que vous metiez votre java à jour");
-         }
-   }
-    
+
+    /**
+     * Find errors in your Java version, operating system and your operating
+     * system version.
+     *
+     */
+    public void errorsOs() {
+        String nameOS = System.getProperty("os.name");
+        String Version = System.getProperty("os.version");
+        String javaVersion = System.getProperty("java.home");
+
+        if (nameOS.contains("Windows") || nameOS.contains("MAC") || nameOS.contains("Linux")) {
+        } else {
+            System.out.println("Votre système d'exploitation n'est pas pris en charge");
+        }
+
+        Pattern p = Pattern.compile("jdk1.8.0_31");
+        Matcher m = p.matcher(javaVersion);
+        if (m.find()) {
+            System.out.println("a jour");;
+        } else {
+            System.out.println("Il faut que vous metiez votre java à jour");
+        }
+    }
+
 }
